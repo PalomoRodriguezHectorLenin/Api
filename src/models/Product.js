1528f1439968fc242542dc6f998184dadd09f80b
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-17 23:44:38 -0600

    Cambiosv2

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,11 +1,11 @@
 import {Schema, model}from 'mongoose'
 
-new Schema({
+const productSchema = new Schema({
     name: String,
     category: String,
     price: Number,
     imgURL: String
-},{
+}, {
     timestamps: true,
     versionKey: false
 })
